a62f89b6a1920db85e2784ec53233bdecbfefd8d
Mark Sarcevicz <smark@broadinstitute.org>
2020-06-22 13:08:43 -0400

    removed insert for now
    fixed apt-get error
    
    added missing bracket
    
    added weird sed for for loop
    
    added debug
    
    changed echo for printf
    
    trying fix
    
    chaged logic for ifs
    
    added version function
    
    removed extra lines
    
    changed function
    
    removed useless line
    
    added spacing
    
    changed order
    
    added print out of chart name and version
    
    syntax
    
    added space
    
    added spacing and new logic to be picked up
    
    revert
    
    added new space
    
    added helm package
    
    added simple tagger

--- src/main.sh
+++ src/main.sh
@@ -15,37 +15,49 @@ parseInputs () {
     echo "Input umbrella_name cannot be empty"
     exit 1
   fi
+}
 
+function version { echo "$@" | awk -F. '{ printf("%d%03d%03d%03d\n", $1,$2,$3,$4); }'; }
 
 helmchartrelease () {
-helm repo add datarepo-helm https://broadinstitute.github.io/datarepo-helm
-helm repo update
-for i in ${helm_charts}
+for i in $(echo $helm_charts | sed "s/,/ /g")
 do
   currentverion=$(yq r charts/${umbrella_name}/Chart.yaml "dependencies.(name==${i}).version")
   newversion=$(yq r charts/${i}/Chart.yaml 'version')
-  if [ "${newversion}" -gt "${newversion}" ]; then
-    printf  "Updating chart verion of ${i} to ${newversion}\n"
-    yq w -i charts/${umbrella_name}/Chart.yaml "dependencies.(name==${i}).version" ${newversion}
-  elif [ "${newversion}" -eq "${newversion}" ]; then
-    printf "No new version for release for ${i}\n"
-  elif [ "${newversion}" -lt "${newversion}" ]; then
-    printf "Current umbrella chart ${i} is ahead of the subchart you may need to release a new subchart version\n"
-  elif [[ "${currentverion}" == "" ]]; then
-    printf "No version of this chart exists in the umbrella chart adding ${i}\n"
+  printf "chartname: ${i}, current: ${currentverion}, new: ${newversion}\n\n"
+  if [[ ${currentverion} == "" ]]; then
+    printf "No version of this chart exists in the umbrella chart adding ${i}\n\n"
     yq w -i charts/${umbrella_name}/Chart.yaml "dependencies[+].name" ${i}
     yq w -i charts/${umbrella_name}/Chart.yaml "dependencies.(name==${i}).version" ${newversion}
     yq w -i charts/${umbrella_name}/Chart.yaml "dependencies.(name==${i}).repository" "https://broadinstitute.github.io/datarepo-helm/"
     yq w -i charts/${umbrella_name}/Chart.yaml "dependencies.(name==${i}).condition" "${i}.enabled"
+    printf "printing new charts/${umbrella_name}/Chart.yaml\n\n"
+    cat charts/${umbrella_name}/Chart.yaml
+  elif [ $(version ${newversion}) -gt $(version ${currentverion}) ]; then
+    printf  "Updating chart verion of ${i} to ${newversion}\n\n"
+    yq w -i charts/${umbrella_name}/Chart.yaml "dependencies.(name==${i}).version" ${newversion}
+    printf "printing new charts/${umbrella_name}/Chart.yaml"
+    cat charts/${umbrella_name}/Chart.yaml
+  elif [ $(version ${newversion}) -eq $(version ${currentverion}) ]; then
+    printf "No new version for release for ${i}\n\n"
+  elif [ $(version ${newversion}) -lt $(version ${currentverion}) ]; then
+    printf "Current umbrella chart ${i} is ahead of the subchart you may need to release a new subchart version\n\n"
   else
-    printf "Error chart updating subchart ${i}\n"
+    printf "Error chart updating subchart ${i}\n\n"
   fi
 done
+}
+
+helmpackage () {
+  helm repo add datarepo-helm https://broadinstitute.github.io/datarepo-helm
+  helm repo update
+  helm package charts/${umbrella_name} --destination .deploy -u
 }
 
 main () {
   parseInputs
   helmchartrelease
+  helmpackage
 }
 
 main "${*}"
